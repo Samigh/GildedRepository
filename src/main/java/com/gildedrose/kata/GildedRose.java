54790218094bb230853baa0aa9f05467296803ed
GHAOUTA Samir <ghaouta.samir@gmail.com>
2017-11-30 12:01:44 +0100

    init Gildedrose-Refactoring project (for Kata)
    Signed-off-by: GHAOUTA Samir <ghaouta.samir@gmail.com>

---
+++ src/main/java/com/gildedrose/kata/GildedRose.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.gildedrose.kata;
+
+/**
+ * @author samirsoft
+ *
+ */
+public class GildedRose {
+
+	public GildedRose() {
+	}
+
+	public void updateQuality(Item item) {
+
+	}
+}
